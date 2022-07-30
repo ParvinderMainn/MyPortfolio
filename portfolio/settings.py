39e4b0048b5318d5213ae1da09870e219f2913b6
ParvinderMainn <76472272+ParvinderMainn@users.noreply.github.com>
2022-07-30 11:58:43 +0530

    Update settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -21,7 +21,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR,'templates')
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$xy@eehgv8yoz@*5n_bhply)!&a@*($qx%48m)_!zg0_@poww5'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
